3f8a3cc647e8b8766e62195b4a0a48343db12807
Dmitry <dementev.ru55@gmail.com>
2026-09-04 18:31:24 +0500

    доработка serializers и создание urls

--- backend/auths/api/serializers.py
+++ backend/auths/api/serializers.py
@@ -11,3 +11,17 @@ class AccountSerializer(serializers.ModelSerializer):
         extra_kwargs = {
             "password": {"write_only": True}
         }
+
+    def validate(self, data):
+        if data["password"] != data["password2"]:
+            raise serializers.ValidationError("The two passwords do not match !")
+        return data 
+    
+    def create(self, validated_data):
+        email = validated_data["email"]
+        password = validated_data["password"]
+        confirm_account = CustomUser.objects.filter(email=email)
+        if confirm_account.exists():
+            raise serializers.ValidationError("An account already exists with this email")
+        new_account = CustomUser.objects.create_user(email=email, password=password)
+        return new_account
