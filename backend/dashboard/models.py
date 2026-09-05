f64b37916eee3f0ffcdafce48471df66415bc193
Dmitry <dementev.ru55@gmail.com>
2026-09-05 13:42:45 +0500

    создание модели для dashboard, доработка apps

--- backend/dashboard/models.py
+++ backend/dashboard/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.utils import timezone
+from auths.models import CustomUser
+from django.utils.text import slugify 
+from django.conf import settings
 
-# Create your models here.
+class JobPost(models.Model):
+    CONTRACT_TYPE = (
+        ("Full-time", "Full-time"),
+        ("Part-time", "Part-time"),
+        ("Freelance", "Freelance"),
+        ("Internship", "Internship")
+    )
+    COMPANY_SIZE = (
+            ("0-10", "0-10"),
+            ("11-50", "11-50"),
+            ("51-200", "51-200"),
+            ("201-500", "201-500"),
+            ("501-1000", "501-1000"),
+            ("1001-5000", "1001-5000")
+        )
+    INDUSTRY = (
+            ("SaaS", "SaaS"),
+            ("E-commerce", "E-commerce"),
+            ("Technology", "Technology"),
+            ("Fashion", "Fashion"), 
+            ("Finance", "Finance"),
+            ("Health", "Health"), 
+            ("Gaming", "Gaming"),
+            ("Travel", "Travel"), 
+            ("Food", "Food"),
+            ("Education", "Education"),
+            ("Transportation", "Transportation"),
+            ("Music", "Music"), 
+            ("Arts", "Arts"), 
+            ("Others", "Others")
+        )
